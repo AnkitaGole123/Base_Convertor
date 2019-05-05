6c3408956b360070509e7ed660d33b49e18ef5c2
Ankitagole <ankita17@navgurukul.org>
2019-05-05 19:21:54 +0530

    Making function to get number in a list

--- OctalToDecimalConverter.java
+++ OctalToDecimalConverter.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class OctalToDecimalConverter {
     public static void main(String[] args) {
         String octal = args[0];
@@ -10,23 +13,28 @@ public class OctalToDecimalConverter {
         boolean notDecimal = true;
         while(octalNumber!=0) {
             lastDigit = getLastDigit(octalNumber);
-
-                octalNumber= getOctalNumber(octalNumber);
-                decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
+            octalNumber= getOctalNumber(octalNumber);
+            decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
                 i++;
         }
         System.out.println("The decimal number of the number you have given is "+decimalNumber);
         }
-
     private static boolean isNotOctal(String octal) {
         return octal.contains("8") || octal.contains("9");
     }
-
     private static int getOctalNumber(int octalNumber) {
         return octalNumber / 10;
     }
     private static int getLastDigit(int octalNumber) {
         return octalNumber % 10;
+    }
 
+    private  static List<Integer> getDigits(int number){
+        List<Integer> digits = new ArrayList<Integer>();
+        while(number!=0){
+            digits.add(getLastDigit(number));
+            number = getOctalNumber(number);
+        }
+        return digits;
     }
 }
