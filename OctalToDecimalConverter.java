3c6f781e9a83fec31d250f89abdb4f7b5b5c1e5a
Ankitagole <ankita17@navgurukul.org>
2019-05-05 13:19:49 +0530

    Revert "Taking variable as string"
    This reverts commit 37c68eb3fe084f952d36101b9ebcd7698d7bfc57.

--- OctalToDecimalConverter.java
+++ OctalToDecimalConverter.java
@@ -1,35 +1,33 @@
 public class OctalToDecimalConverter {
     public static void main(String[] args) {
-        String octal = args[0];
-        System.out.println(octal);
-//        int octalNumber = Integer.parseInt(args[0]);
-//        int i=0,decimalNumber=0,lastDigit,exponent=8;
-//        boolean notDecimal = true;
-//        while(octalNumber!=0) {
-//            lastDigit = getLastDigit(octalNumber);
-//            if(lastDigit > 7) {
-//                notDecimal = false;
-//                System.out.println("your input is not an OctalToDecimalConverter number");
-//                break;
-//            }
-//            else{
-//                octalNumber= getOctalNumber(octalNumber);
-//                decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
-//                i++;
-//            }
-//        }
-//        if (notDecimal==true){
-//            System.out.println("The decimal number of the number you have given is "+decimalNumber);
-//        }
-//
-//    }
-//
-//    private static int getOctalNumber(int octalNumber) {
-//        return octalNumber / 10;
-//    }
-//
-//    private static int getLastDigit(int octalNumber) {
-//        return octalNumber % 10;
+        int octalNumber = Integer.parseInt(args[0]);
+        int i=0,decimalNumber=0,lastDigit,exponent=8;
+        boolean notDecimal = true;
+        while(octalNumber!=0) {
+            lastDigit = getLastDigit(octalNumber);
+            if(lastDigit > 7) {
+                notDecimal = false;
+                System.out.println("your input is not an octal number");
+                break;
+            }
+            else{
+                octalNumber= getOctalNumber(octalNumber);
+                decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
+                i++;
+            }
+        }
+        if (notDecimal==true){
+            System.out.println("The decimal number of the number you have given is "+decimalNumber);
+        }
+
+    }
+
+    private static int getOctalNumber(int octalNumber) {
+        return octalNumber / 10;
+    }
+
+    private static int getLastDigit(int octalNumber) {
+        return octalNumber % 10;
 
         }
 
