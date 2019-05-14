7bc1b74cf8b5088aeb1eeca3b7fd1d5f1310ddc1
Ankitagole <ankita17@navgurukul.org>
2019-05-14 13:56:07 +0530

    changing expectedOutput files name

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 javac OctalToDecimalConverter.java
 java OctalToDecimalConverter --base 8 123 > actual
-diff actual ExpectedOutput
+diff actual ExpectedOutputOfBase8
 
-java OctalToDecimalConverter --base 8 1 > actual
-diff actual ExpectedOutput1
+java OctalToDecimalConverter --base 5 1 > actual
+diff actual ExpectedOutputOfBase5
 
 java OctalToDecimalConverter --base 2 34 > actual
-diff actual ExpectedOutput2
+diff actual ExpectedOutputOfBase2
