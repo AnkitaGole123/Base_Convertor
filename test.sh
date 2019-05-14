46360d65f58f6b96a905ba7094693fe670a94e54
Ankitagole <ankita17@navgurukul.org>
2019-05-14 13:20:00 +0530

    changing expected output

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 javac OctalToDecimalConverter.java
-java OctalToDecimalConverter --base 7 123 > actual
+java OctalToDecimalConverter --base 8 123 > actual
 diff actual ExpectedOutput
 
-java OctalToDecimalConverter --base 7 1 > actual
+java OctalToDecimalConverter --base 8 1 > actual
 diff actual ExpectedOutput1
 
-java OctalToDecimalConverter --base 7 11 > actual
+java OctalToDecimalConverter --base 2 34 > actual
 diff actual ExpectedOutput2
